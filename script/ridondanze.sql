1fdac5cb8887bdcfe43713faa424c1839ff5c8b3
Federico Minniti <93187746+federicominniti@users.noreply.github.com>
2022-03-01 22:39:31 +0100

    Update ridondanze.sql

--- script/ridondanze.sql
+++ script/ridondanze.sql
@@ -7,9 +7,9 @@ CREATE FUNCTION mediaKmPool2(_targa CHAR(7))
 RETURNS FLOAT DETERMINISTIC
 BEGIN
 	DECLARE media FLOAT;
-    SET media = (SELECT NkmPercorsi/NPool
-				 FROM AUTOVETTURA A
-				 WHERE A.Targa=_targa);
+    	SET media = (SELECT NkmPercorsi/NPool
+			 FROM AUTOVETTURA A
+			 WHERE A.Targa=_targa);
 	RETURN(media);
 END$$
 
@@ -27,8 +27,8 @@ AFTER INSERT ON PERCORRE
 FOR EACH ROW
 BEGIN
 	DECLARE nKm INT;
-    SET nKm=kmTragitto(NEW.CodiceTragitto);
-    UPDATE AUTOVETTURA SET NkmPercorsi=NkmPercorsi+nKm WHERE Targa=NEW.Targa;
+    	SET nKm=kmTragitto(NEW.CodiceTragitto);
+    	UPDATE AUTOVETTURA SET NkmPercorsi=NkmPercorsi+nKm WHERE Targa=NEW.Targa;
 END$$
 
 /*5.3.9*/
@@ -38,34 +38,34 @@ CREATE FUNCTION media_recensioni_totale(_utente VARCHAR (15))
 RETURNS FLOAT DETERMINISTIC
 BEGIN
 	DECLARE appPunteggio INT DEFAULT 0;
-    DECLARE appNRecensioni INT DEFAULT 0;
-    DECLARE nRecensioni INT DEFAULT 0;
+    	DECLARE appNRecensioni INT DEFAULT 0;
+    	DECLARE nRecensioni INT DEFAULT 0;
 	DECLARE media FLOAT DEFAULT 0;
     
 	SELECT PunteggioRecensioni, NumeroRecensioni INTO appPunteggio, appNRecensioni
 	FROM PROPONENTE P
 	WHERE P.NomeUtenteP=_utente;
     
-    SET nRecensioni = nRecensioni + appNRecensioni;
-    SET media = media + appPunteggio;
+    	SET nRecensioni = nRecensioni + appNRecensioni;
+    	SET media = media + appPunteggio;
     
-    SET appNRecensioni=0;
-    SET appPunteggio=0;
+    	SET appNRecensioni=0;
+    	SET appPunteggio=0;
     
-    SELECT PunteggioRecensioni, NumeroRecensioni INTO appPunteggio, appNRecensioni
+    	SELECT PunteggioRecensioni, NumeroRecensioni INTO appPunteggio, appNRecensioni
 	FROM FRUITORE F
 	WHERE F.NomeUtenteF=_utente;
     
-    SET nRecensioni = nRecensioni + appNRecensioni;
-    SET media = media + appPunteggio;
+    	SET nRecensioni = nRecensioni + appNRecensioni;
+    	SET media = media + appPunteggio;
     
-    IF nRecensioni != 0 THEN
+    	IF nRecensioni != 0 THEN
 		SET media = media/(nRecensioni*4);
 	ELSE
 		SET media = -1;
-    END IF;
+    	END IF;
     
-    RETURN(media);
+    	RETURN(media);
 END$$
 
 DROP TRIGGER IF EXISTS aggiorna_punteggi_utenti$$
@@ -74,12 +74,12 @@ AFTER INSERT ON VALUTAZIONE
 FOR EACH ROW
 BEGIN
     IF NEW.Flag = 1 THEN
-		UPDATE FRUITORE SET PunteggioRecensioni=PunteggioRecensioni+NEW.Persona+NEW.Comportamento+NEW.Serieta+NEW.PiacereViaggio,
-							NumeroRecensioni = NumeroRecensioni + 1
+	UPDATE FRUITORE SET PunteggioRecensioni=PunteggioRecensioni+NEW.Persona+NEW.Comportamento+NEW.Serieta+NEW.PiacereViaggio,
+		NumeroRecensioni = NumeroRecensioni + 1
         WHERE NomeUtenteF=NEW.NomeUtenteF;
 	ELSE
 		UPDATE PROPONENTE SET PunteggioRecensioni=PunteggioRecensioni+NEW.Persona+NEW.Comportamento+NEW.Serieta+NEW.PiacereViaggio,
-							  NumeroRecensioni = NumeroRecensioni + 1
-        WHERE NomeUtenteP=NEW.NomeUtenteP;
+			NumeroRecensioni = NumeroRecensioni + 1
+        	WHERE NomeUtenteP=NEW.NomeUtenteP;
 	END IF;
 END$$
